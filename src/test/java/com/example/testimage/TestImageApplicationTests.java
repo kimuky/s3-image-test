884bea57bf0f7986d04998dd646a045b56365cc4
kimuky <xogus559@gmail.com>
2025-01-02 10:29:08 +0900

    test 1

--- src/test/java/com/example/testimage/TestImageApplicationTests.java
+++ src/test/java/com/example/testimage/TestImageApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.testimage;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TestImageApplicationTests {
 
     @Test
